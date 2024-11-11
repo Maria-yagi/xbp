0329331a9f18562b34bfdb4c387a365bf8d40b39
maria-yagi <r202402234mz@jindai.jp>
2024-11-11 20:51:51 +0900

    20241111

--- de12/python/bomgame.py
+++ de12/python/bomgame.py
@@ -1,18 +1,20 @@
 import random
 choices =["赤色","青色","黄色"]
 
+correct_choices = random.choices
+
 print("爆弾を止めよう！")
-print("0:赤色 ,1:青色 ,2:黄色")
+print("次の中からコードの色を選ぼう:赤色 ,1:青色 ,2:黄色")
 
 while True:
-        user_choice =int("あなたの運命を決めましょう！（0,1,2):")
-        computer_choice=random.randint(0,2)
-        print(f"あなた:{choices[user_choice]},  ボンバーマン: {choices[computer_choice]}")
+    guess = input ("あなたの選択した色")
+        if guess == correct_choices:
+            print("死亡です！　ゲームオーバー")
+            break
+            
+        else :
+            print("生存しました！ あと1本！　確率は２分の１　\n")
+        
 
-        if user_choice==computer_choice:
-            print("死亡　残念")
-        elif (user_choice ==0 and computer_choice==1)
-             (user_choice ==0 and computer_choice==2)
-             (user_choice ==1 and computer_choice==0)
-             (user_choice ==1 and computer_choice==2)
-             (user_choice ==2 and computer_choice)
+        
+        
